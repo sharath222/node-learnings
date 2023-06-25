1028b09239d9d1dcb8ee6223aa3e503ba0781b5d
Sharath Chandra <chandra.sharath811@gmail.com>
2023-06-25 19:57:47 +0530

    2 chapters done

---
+++ asynchnous.js
@@ -0,0 +1,19 @@
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('done')
+        }, 1500);
+    })
+    return promise;
+}
+
+setTimeout(() => {
+    console.log('time done');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    })
+        .then(text2 => {
+            console.log((text2));
+        })
+}, 2000)
--- first-app.js
+++ first-app.js
@@ -1,5 +1,52 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('hello.txt', 'hello.js')
+// fs.writeFileSync('hello.txt', 'hello.js')
 
-console.log('git commit');
+
+/**
+ *
+ * @param {*} user
+ * @returns arrow functions
+ */
+// const user = user => user;
+
+// console.log(user('sharath'));
+
+/**
+ * objects
+ */
+// const person = {
+//     name: 'sharath',
+//     age: 28,
+//     combine() {
+//         return this.name + this.age
+//     }
+// }
+
+// console.log(person.combine())
+
+/**
+ * arrays
+ */
+// const x = ['a', 'b']
+// const y = [1,2]
+// const copied = [...x, ...y]
+
+/**
+ * destructuring
+ */
+// const person = {
+//     name: 'sharath',
+//     age: 28,
+//     combine() {
+//         return this.name + this.age
+//     }
+// }
+// const fp = ({age}) => {
+//     console.log(age);
+// }
+
+// fp(person)
+
+// const {name, age} = person
+// console.log(name, age);
